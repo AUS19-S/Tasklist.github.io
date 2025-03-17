d5946597f0e46a146696e4435c67afbb3de2fe4d
Silanda <silanda@aluno.unilab.edu.br>
2025-03-16 23:02:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -44,8 +44,67 @@ function selecionarTarefa(){
     if(aux == numH5 - 1){
         aux = -1;
     }
-    
+
     h5[aux + 1].className = "tarefaSelecionada";
 }
 let btSelecionar = document.getElementById("btSelecionar");
 btSelecionar.addEventListener("click", selecionarTarefa);
+
+function retirarSelecionada(){
+    // cria referência de um filho que irá perder um filho;
+    let divQuadro = document.getElementById("divQuadro");
+    let h5 = document.getElementsByTagName("h5");
+    let numH5 = h5.length;
+
+    let aux = -1;
+
+    for(let i = 0; i < numH5; i++){
+        if(h5[i].className == "tarefaSelecionada"){
+            aux = i;
+            break;
+        }
+    }
+    if(aux == -1){
+        alert("Selecione uma tarefa para remover.");
+        return;
+    }
+    if(confirm("Confirma Exclusão de " + h5[aux].textContent + "?")){
+        divQuadro.removeChild(h5[aux]);
+    }
+}
+
+let btRetirar = document.getElementById("btRetirar");
+btRetirar.addEventListener("click", retirarSelecionada);
+
+function gravarTarefa(){
+    let h5 = document.getElementsByTagName("h5");
+    let numH5 = h5.length;
+    if(numH5 == 0){
+        alert("Não há tarefa para serem salvas");
+        return;
+    }
+    let tarefas = "";
+    for(let i = 0; i < numH5; i++){
+        tarefas += h5[i].textContent + ";";
+    }
+    localStorage.setItem("TarefasDias", tarefas.substring(0, tarefas.length - 1));
+    if(localStorage.getItem("TarefasDias")){
+        alert("Ok, Tarefas Salvas");
+    }
+}
+let btGravar = document.getElementById("btGravar");
+btGravar.addEventListener("click", gravarTarefa);
+
+function recuperarTarefasSalvas(){
+    if(localStorage.getItem("TarefasDias")){
+        let tarefas = localStorage.getItem("TarefasDias").split(";");
+        let divQuadro = document.getElementById("divQuadro");
+        for(let i = 0; i < tarefas.length; i++){
+            let h5 = document.createElement("h5");
+            let texto = document.createTextNode(tarefas[i]);
+            h5.appendChild(texto);
+            divQuadro.appendChild(h5);
+        }
+    }
+}
+recuperarTarefasSalvas();
